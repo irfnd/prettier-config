8b3b310668a0c0c6e42942a2dc43ba094f423cfa
Irfandi Iqbal Abimanyu <irfndiabimanyu@gmail.com>
2025-05-19 21:56:20 +0700

    feat: hybrid packages to support es module and commonjs

---
+++ dist/cjs/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var config = {
+    endOfLine: 'lf',
+    jsxSingleQuote: true,
+    printWidth: 130,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: true,
+};
+exports.default = config;
